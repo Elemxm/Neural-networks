4d97c126386c2c82382c3728df25e608225aebc3
Elemxm <elenamaxmoutai@gmail.com>
2023-11-26 11:28:02 +0200

    Added our predictions

---
+++ final_assignment/nn_second_experiment_fixed_overfitting.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from keras import layers, datasets
+from keras.callbacks import TensorBoard
+import time
+
+layer_sizes = [10, 64, 128, 256]
+
+# Loading the CIFAR-10 dataset
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Defining the class names
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+# This network has 2 hidden layers; they both are dense layers and have relu activation
+for layer1_size in layer_sizes:
+    for layer2_size in layer_sizes:
+        NAME = "{}-layer1-{}-layer2-{}".format(layer1_size, layer2_size, int(time.time()))
+        tensorboard = TensorBoard(log_dir='D:/old-files/Desktop/thmmu/9o/NeurwnikaDiktua/assignment_logs/{}'.format(NAME))
+        model = tf.keras.models.Sequential()
+
+        # Flatten layer
+        model.add(layers.Conv2D(layer1_size, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+        model.add(layers.Dropout(0.5))  # Dropout layer after the first Conv2D layer
+
+        model.add(layers.Conv2D(layer2_size, (3, 3), activation='relu'))
+        model.add(layers.Dropout(0.5))  # Dropout layer after the second Conv2D layer
+
+        model.add(layers.Flatten())
+        model.add(layers.Dense(10, activation='softmax'))
+
+        # Compile the model
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        # Train the model
+        model.fit(train_images, train_labels, epochs=10, validation_split=0.3, callbacks=[tensorboard])
+
+        # Evaluate the model on the test set
+        test_loss, test_acc = model.evaluate(test_images, test_labels)
+        print(f'Test loss: {test_loss}')
+        print(f'Test accuracy: {test_acc}')
---
+++ final_assignment/nn_second_experiment_predictions.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+from keras import layers, models, datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.callbacks import TensorBoard
+import time
+
+NAME = "256-layer1-256-layer2-{}".format(int(time.time()))
+TensorBoard = TensorBoard(log_dir= 'D:/old-files/Desktop/thmmu/9o/NeurwnikaDiktua/assignment_logs/{}'.format(NAME))
+
+# Loading the CIFAR-10 dataset
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+#Defining the class names
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+# Build the neural network model
+model = tf.keras.models.Sequential()
+model.add(layers.Conv2D(128, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.Dropout(0.5))  # Dropout layer after the first Conv2D layer
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.Dropout(0.5))  # Dropout layer after the second Conv2D layer
+
+model.add(layers.Flatten())
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',  # Use 'sparse_categorical_crossentropy' for integer labels
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=10, validation_split=0.3, callbacks=[TensorBoard])
+
+# Evaluate the model on the test set
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f'Test loss: {test_loss}')
+print(f'Test accuracy: {test_acc}')
+
+model.save('first_experiment')
+new_model = tf.keras.models.load_model('second_experiment')
+predictions = new_model.predict([test_images])
+
+# Function to display images along with their predicted and actual labels
+def plot_images(images, predicted_labels, actual_labels, class_names):
+    plt.figure(figsize=(10, 10))
+    for i in range(min(25, len(images))):
+        plt.subplot(5, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(images[i], cmap=plt.cm.binary)
+
+        predicted_label = np.argmax(predicted_labels[i])
+        actual_label = actual_labels[i, 0]
+
+        plt.xlabel(f'Predicted: {class_names[predicted_label]} Actual: {class_names[actual_label]}')
+
+    plt.show()
+
+# Select a random subset of test images
+num_samples = 25
+indices = np.random.choice(len(test_images), num_samples, replace=False)
+sample_images = test_images[indices]
+sample_labels = test_labels[indices]
+
+# Make predictions on the selected subset
+sample_predictions = new_model.predict(test_images[indices])
+
+# Plot the images with predictions and actual labels
+plot_images(sample_images, sample_predictions, sample_labels, class_names)
