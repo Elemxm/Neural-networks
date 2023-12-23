a0fc468f371df47ccdde0d1683404bc0574c0416
Elemxm <elenamaxmoutai@gmail.com>
2023-12-23 10:41:39 +0200

    erased extra functions from the code

--- second_assignment/rbf_svm_plot.py
+++ second_assignment/rbf_svm_plot.py
@@ -6,7 +6,7 @@ with open('second_assignment\svm_results.txt', 'r') as file:
     data = file.readlines()
 
 # Initialize lists to store extracted data
-C_values, gamma_values, accuracy_train, accuracy_test = [], [], [], []
+C_values, gamma_values, accuracy_train, accuracy_test, training_time, prediction_time_train, prediction_time_test  = [], [], [], [], [], [], []
 
 # Extract data from each line
 for line in data:
@@ -22,12 +22,26 @@ for line in data:
 
     if "The accuracy score on testing data is:" in line:
         accuracy_test.append(float(line.split(":")[1]))
+   
+    if "Training time:" in line:
+        training_time.append(float(line.split(":")[1].split(" seconds")[0]))
+
+    if "Prediction time for train images:" in line:
+        prediction_time_train.append(float(line.split(":")[1].split(" seconds")[0]))
+
+    if "Prediction time for test images:" in line:
+        prediction_time_test.append(float(line.split(":")[1].split(" seconds")[0]))
+
 
 # Convert lists to numpy arrays for better manipulation
 C_values = np.array(C_values)
 gamma_values = np.array(gamma_values)
 accuracy_train = np.array(accuracy_train)
 accuracy_test = np.array(accuracy_test)
+training_time = np.array(training_time)
+prediction_time_train = np.array(prediction_time_train)
+prediction_time_test = np.array(prediction_time_test)
+
 
 # Define a color map for discrete gamma values
 gamma_colors = {0.001: 'red', 0.01: 'green', 0.1: 'blue', 1: 'purple', 10: 'orange', 100: 'brown'}
@@ -52,50 +66,9 @@ def ploting_func(unique_gamma_values, C_values, accuracy, gamma_colors, type_of_
     plt.legend()
     plt.show()
 
-type_of_data = ['Training', 'Testing']
-ploting_func(unique_gamma_values, C_values, accuracy_train, gamma_colors, type_of_data[0])
-ploting_func(unique_gamma_values, C_values, accuracy_test, gamma_colors, type_of_data[1])
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Read data from the file
-with open('second_assignment\svm_results.txt', 'r') as file:
-    data = file.readlines()
-
-# Initialize lists to store extracted data
-C_values, gamma_values, training_time, prediction_time_train, prediction_time_test = [], [], [], [], []
-
-# Extract data from each line
-for line in data:
-    if "SVC Linear Classification results" in line:
-        # Extract C and gamma values
-        C, gamma = map(float, line.split("for C = ")[1].split(" and gamma = ")[0:2])
-        C_values.append(C)
-        gamma_values.append(gamma)
-
-    # Extract time values
-    if "Training time:" in line:
-        training_time.append(float(line.split(":")[1].split(" seconds")[0]))
-
-    if "Prediction time for train images:" in line:
-        prediction_time_train.append(float(line.split(":")[1].split(" seconds")[0]))
-
-    if "Prediction time for test images:" in line:
-        prediction_time_test.append(float(line.split(":")[1].split(" seconds")[0]))
-
-# Convert lists to numpy arrays for better manipulation
-C_values = np.array(C_values)
-gamma_values = np.array(gamma_values)
-training_time = np.array(training_time)
-prediction_time_train = np.array(prediction_time_train)
-prediction_time_test = np.array(prediction_time_test)
-
-# Define a color map for discrete gamma values
-gamma_colors = {0.001: 'red', 0.01: 'green', 0.1: 'blue', 1: 'purple', 10: 'orange', 100: 'brown'}
-
-# Get unique gamma values
-unique_gamma_values = np.unique(gamma_values)
+ploting_func(unique_gamma_values, C_values, accuracy_train, gamma_colors, 'Training')
+ploting_func(unique_gamma_values, C_values, accuracy_test, gamma_colors, 'Testing')
+  
 
 def ploting_func(unique_gamma_values, C_values, time_data, gamma_colors, time_type):
     plt.figure(figsize=(12, 6))
